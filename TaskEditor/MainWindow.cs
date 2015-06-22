c668570c63d8e44fcd53548b910a4620ea8dfbb3
farkmischer <farkmischer@gmail.com>
2015-06-21 19:02:58 -0500

    More Initial Changes

---
+++ TaskEditor/MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+using DataContracts;
+using Managers;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Result result = new TestManager().RunEntityExample();
+
+		Build ();
+	}
+
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
